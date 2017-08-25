d7cc8bc0ee6af1c70b6a17cea2f44c3f6d1ac2b6
Absner <absrraniel@gmail.com>
2017-08-25 17:09:10 -0400

    construyendo modales

---
+++ js/main.js
@@ -0,0 +1,27 @@
+$('#exampleModal').on('shown.bs.modal', function () {
+
+})
+
+$('#exampleModal2').on('shown.bs.modal', function () {
+
+})
+
+$('#exampleModal3').on('shown.bs.modal', function () {
+
+})
+
+$('#exampleModal4').on('shown.bs.modal', function () {
+
+})
+
+$('#exampleModal5').on('shown.bs.modal', function () {
+
+})
+
+$('#exampleModal6').on('shown.bs.modal', function () {
+
+})
+
+$('#exampleModal7').on('shown.bs.modal', function () {
+
+})
